6184e5b2930fcecca6314bf97367d0af02f26164
ryan <ryan@admins-MBP.home>
2024-05-23 18:12:25 -0700

    Passing tests for pricing engine

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,8 +1,11 @@
 module BookingsHelper
     def calculate_total_cost(animal_type, hours_requested)
-        base_rate = 20.0
-        animal_rate = animal_type == 'Dog' ? 10 : 5
-        base_rate + (animal_rate * hours_requested)
+      raise ArgumentError, "Invalid animal type" unless %w[Dog Cat].include?(animal_type)
+      raise ArgumentError, "Hours requested cannot be negative" if hours_requested.negative?
+  
+      base_rate = 20.0
+      animal_rate = animal_type == 'Dog' ? 10 : 5
+      base_rate + (animal_rate * hours_requested)
     end
-end
+  end
   
